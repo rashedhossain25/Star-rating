b03afd8f1a2bfad6da17c27f6bd0aac2556adb11
Rashed Hossain <145587722+rashedhossain25@users.noreply.github.com>
2024-01-03 13:32:25 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+//jq code
+$(document).ready(function(){
+
+  $('.rating').starRating({
+    starIconEmpty: 'far fa-star',
+    starIconFull: 'fas fa-star',
+    starColorEmpty: 'lightgray',
+    starColorFull: '#FFC107',
+    starsSize: 4, // em
+    stars: 5,
+  });
+
+  });
